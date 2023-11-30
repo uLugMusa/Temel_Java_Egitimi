b974d28324aa171c47e4f8d4433346b599e23520
Musa Ulug <ulugmusa56@gmail.com>
2023-12-01 01:56:56 +0300

    yeniden ekleme
    yeniden ekleme

---
+++ Java_004_Donguler/src/main/java/ulug/musa/_001_For.java
@@ -0,0 +1,20 @@
+package ulug.musa;
+
+public class _001_For
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("---------FOR---------");
+        for(int i = 0; i<10 ; i++)
+        {
+            System.out.println(i);
+
+        }
+
+    }
+
+
+}
+
+
